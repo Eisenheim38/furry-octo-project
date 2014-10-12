1539eccf451a687ae504fca20197dd41d3622709
Krystle <acuarito@gmail.com>
2014-10-12 00:48:08 -0430

    Boceto

---
+++ project.m
@@ -0,0 +1,15 @@
+% pseudo-solución
+%
+% Entrada: Matriz de adyacencia A
+%
+% Proceso:
+%   Nota: Evaluar si el grafo que representa la matriz A posee más de una
+%       componente conexa (si es así algo debe cambiar).
+%   Paso 1: Obtener matriz con promedio ponderado
+%       Lo explica la presentación...
+%   Paso 2: Verificar que la matriz sea estocástica izquierda
+%       (no poseerá números negativos y la suma de sus columnas será 1)
+%   Paso 3: Obtener el autovector asociado con el mayor autovalor
+%       Usar método de iterativo de las potencias.
+%
+% Salida: Vector de nodos ordenados por importancia
