913f6497ea1c813747bed2def28bfa108003dfd5
Krystle <acuarito@gmail.com>
2014-10-12 18:16:52 -0430

    Cambios menores

--- main.m
+++ main.m
@@ -1,25 +1,27 @@
-% pseudo-soluci√≥n, copia con modificacion de "krysProject.m"
+% pseudo-soluciÛn, copia con modificacion de "krysProject.m"
 %
 % Entrada: Matriz de adyacencia A
 %
 % Proceso:
-%   Nota: Evaluar si el grafo que representa la matriz A posee m√°s de una
-%       componente conexa (si es as√≠ algo debe cambiar).
-%
+%   Nota: Evaluar si el grafo que representa la matriz A posee m·s de una
+%        una componente conexa (si es asÌ algo debe cambiar...
+%       øaplicar el algoritmo para cada componente?).
 %   Paso 1: Obtener matriz con promedio ponderado
 %       Lo explica la presentaci√≥n...
 %       M = obtenerEstocastica(A);
-%
-%   Paso 3: Obtener el autovector asociado con el mayor autovalor
-%       Usar m√©todo de iterativo de las potencias.
+%   Paso 2: Obtener el autovector asociado con el mayor autovalor
+%       Usar mÈtodo de iterativo de las potencias.
 %	x = metodoDePotencias(M);
 %
 % Salida: Vector de nodos ordenados por importancia
 
-% Caso de prueba con una sola componente conexa (presentaci√≥n)
+% Caso de prueba con una sola componente conexa (presentaciÛn)
 A1 = [0 0 1 1; 1 0 0 0; 1 1 0 1; 1 1 0 0]
 
 % Caso de prueba con dos componente conexas (presentaci√≥n)
 A2 = [0 1 0 0 0; 1 0 0 0 0; 0 0 0 1 1; 0 0 1 0 1; 0 0 0 0 0]
 
-obtenerEstocastica(A1);
+M = obtenerEstocastica(A1);
+M = obtenerEstocastica(A2);
+%x= metodoDePotencias(A)
+%sort(x,'descend')
--- obtenerEstocastica.m
+++ obtenerEstocastica.m
@@ -1,8 +1,5 @@
-% obtener matriz estocastica
-% hints: M = (1-m)*A + m*S
-% where 0 ≤ m ≤ 1 and S is n by n,
-% and S = [1/n...,1/n;...]
-
+% obtener matriz con el promedio ponderado (con la propiedad 
+% de que es una matriz estocastica)
 function M = obtenerEstocastica(A)
 	v = size(A);
     n = v(1);
