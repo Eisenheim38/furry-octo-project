853e0ea2193c60a54ddcfc48698f1dc213670b44
Eisenheim38 <avilancj@gmail.com>
2014-10-17 15:41:48 -0430

    Corregida división entre 0

--- obtenerEstocastica.m
+++ obtenerEstocastica.m
@@ -13,7 +13,11 @@ function A_est = obtenerEstocastica(A)
     % Sacando la matriz estocástica de A
     for i=1:1:n
         for j=1:1:n
-            A_est(i,j) = A(i,j)/y(j);
+			if y(j)==0
+				A_est(i,j) = 0;
+			else
+            	A_est(i,j) = A(i,j)/y(j);
+			end
         end
     end
     A_est
